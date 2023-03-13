1082ea2425ff40c35354565d9ff1dc929a4a253e
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-03-13 11:49:46 +0530

    styled

---
+++ components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+const Weather = ({data}) => {
+console.log(data);
+  return (
+    <div>
+      <div>
+        <div>
+          <Image src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt='/' width='100' height='100' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Weather
