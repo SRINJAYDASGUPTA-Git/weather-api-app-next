7ff020d860e910701503f0f0cfc12439e4a2a12a
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-03-14 14:13:46 +0530

    edit

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -16,7 +16,7 @@ const Weather = ({ data }) => {
         </div>
         <p className="text-9xl">{data.main.temp.toFixed(0)}&#176;F</p>
       </div>
-      <div className="bg-black/50 relative p-8 rounded-md">
+      <div className="bg-black/30 relative p-8 rounded-md">
         <p className="text-2xl text-center pb-6">Weather in {data.name}</p>
         <div className="flex justify-between text-center">
           <div>
