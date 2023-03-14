ab63ba6a281084a09399011b9f00e6ff7b975413
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-03-14 10:57:23 +0530

    added vercel

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
-const Weather = ({data}) => {
-console.log(data);
+import React from "react";
+import Image from "next/image";
+const Weather = ({ data }) => {
+  console.log(data);
   return (
-    <div>
+    <div className="relative">
       <div>
-        <div>
-          <Image src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt='/' width='100' height='100' />
+        <div className="">
+          <Image
+            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            alt="/"
+            width="100"
+            height="100"
+          />
+          <p>{data.weather[0].main}</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
